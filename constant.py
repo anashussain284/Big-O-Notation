68102180fe06d49687e0678fbd2090f1d36f1ec2
Anas Hussain M <anashussain284@gmail.com>
2024-02-03 16:18:59 +0530

    constant timecomplexity eg:program

---
+++ constant.py
@@ -0,0 +1,7 @@
+"""
+The program exhibits constant time complexity, as it runs only once, resulting in a time complexity of O(1). This means that regardless of the input size, the execution time remains constant, making it an efficient and predictable algorithm for this particular scenario.
+"""
+def greeting(name):
+    print("Hello", name) # O(1)
+
+greeting("Anas")
